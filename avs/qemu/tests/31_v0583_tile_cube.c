7331c0d69532b9eaebf14814f5db5fd0fc5a62b0
zhoubot <637081+zhoubot@users.noreply.github.com>
2026-08-23 06:22:20 +0800

    avs: separate CUBE input and output strides

--- avs/qemu/tests/31_v0583_tile_cube.c
+++ avs/qemu/tests/31_v0583_tile_cube.c
@@ -2,7 +2,12 @@
 
 #include <stdint.h>
 
-enum { kDim = 4, kElements = kDim * kDim, kRowStrideBytes = kDim * sizeof(uint8_t) };
+enum {
+    kDim = 4,
+    kElements = kDim * kDim,
+    kInputRowStrideBytes = kDim * sizeof(uint8_t),
+    kOutputRowStrideBytes = kDim * sizeof(uint32_t),
+};
 static const uint32_t kTestId = 0x00002D01u;
 
 static uint8_t lhs[kElements] __attribute__((aligned(64)));
@@ -10,7 +15,8 @@ static uint8_t rhs[kElements] __attribute__((aligned(64)));
 static uint32_t output[kElements] __attribute__((aligned(64)));
 
 extern void linx_v0583_cube_matmul(const uint8_t *a, const uint8_t *b,
-                                   uint32_t *dst, uint64_t row_stride_bytes);
+                                   uint32_t *dst, uint64_t input_row_stride_bytes,
+                                   uint64_t output_row_stride_bytes);
 
 void run_tile_v0583_cube_tests(void)
 {
@@ -25,7 +31,8 @@ void run_tile_v0583_cube_tests(void)
         }
     }
 
-    linx_v0583_cube_matmul(lhs, rhs, output, kRowStrideBytes);
+    linx_v0583_cube_matmul(lhs, rhs, output, kInputRowStrideBytes,
+                           kOutputRowStrideBytes);
 
     for (unsigned i = 0; i < kElements; ++i) {
         TEST_EQ32(output[i], (uint32_t)rhs[i], kTestId);
